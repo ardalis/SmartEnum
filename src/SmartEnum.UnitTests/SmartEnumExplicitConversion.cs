361fa4cef5049451faf7d23b929220d81f255a99
= <=>
2018-02-17 12:17:29 -0700

    Add explicit operator

---
+++ src/SmartEnum.UnitTests/SmartEnumExplicitConversion.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+
+namespace SmartEnum.UnitTests
+{
+    public class SmartEnumExplicitConversion
+    {
+        [Fact]
+        public void ReturnsEnumFromGivenValue()
+        {
+            var result = (TestEnum) 1;
+
+            Assert.Equal(TestEnum.One, result);
+        }
+    }
+}
