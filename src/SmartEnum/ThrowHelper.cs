18b8f0cdbf24a299bb5b6d941c2189f30fbd7026
Antao Almada <antao.almada@gmail.com>
2019-02-28 10:33:36 +0000

    Add ThrowHelper so that methods can be inlined.

---
+++ src/SmartEnum/ThrowHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Ardalis.SmartEnum
+{
+    static class ThrowHelper
+    {
+        public static void ThrowArgumentNullException(string paramName)
+            => throw new ArgumentNullException(paramName);
+
+        public static void ThrowArgumentNullOrEmptyException(string paramName)
+            => throw new ArgumentException("Argument cannot be null or empty.", paramName);
+
+        public static void ThrowNameNotFoundException<TEnum, TValue>(string name)
+            where TEnum : SmartEnum<TEnum, TValue>
+            where TValue : IEquatable<TValue>, IComparable<TValue>
+            => throw new SmartEnumNotFoundException($"No {typeof(TEnum).Name} with Name \"{name}\" found.");
+
+        public static void ThrowValueNotFoundException<TEnum, TValue>(TValue value)
+            where TEnum : SmartEnum<TEnum, TValue>
+            where TValue : IEquatable<TValue>, IComparable<TValue>
+            => throw new SmartEnumNotFoundException($"No {typeof(TEnum).Name} with Value {value} found.");
+    }
+}
