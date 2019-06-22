d432a02e42822dddbf27de341afa9d1cd5c7a00b
Marcel Juen <marcel.juen@raincoat.systems>
2019-06-22 18:41:33 +0200

    Added SmartEnum.EFCore + SmartEnum.EFCore.UnitTests
    The additional package makes it possible to configure all SmartEnums for Entity Framework Core with one single line of code.

---
+++ src/Ardalis.SmartEnum.EFCore.UnitTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Ardalis.SmartEnum.EFCore.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
