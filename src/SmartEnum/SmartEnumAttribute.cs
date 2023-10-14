3ff86368729b970809334ce1d6145ecefb4ef076
Scott DePouw <scott.depouw@gmail.com>
2023-10-14 10:45:50 -0400

    Add SmartEnum Validation attribute, with tests.

---
+++ src/SmartEnum/SmartEnumAttribute.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+namespace Ardalis.SmartEnum
+{
+    /// <summary>
+    /// A <see cref="ValidationAttribute"/> that ensures the provided value matches the
+    /// <see cref="SmartEnum{TEnum}.Name"/> of a <see cref="SmartEnum{TEnum}"/>/<see cref="SmartEnum{TEnum,TValue}"/>.
+    /// Nulls and non-<see cref="string"/> values are considered valid
+    /// (add <see cref="RequiredAttribute"/> if you want the field to be required).
+    /// </summary>
+    public class SmartEnumAttribute : ValidationAttribute
+    {
+        private readonly bool _allowCaseInsensitiveMatch;
+        private readonly Type _smartEnumType;
+
+        /// <param name="smartEnumType">The expected SmartEnum type.</param>
+        /// <param name="propertyName">The name of the property that the attribute is being used on.</param>
+        /// <param name="allowCaseInsensitiveMatch">
+        ///     Unless this is true, only exact case matching the
+        ///     <see cref="SmartEnum{TEnum}" /> Name will validate.
+        /// </param>
+        /// <param name="errorMessage">
+        ///     Message template to show when validation fails. {0} is <paramref name="propertyName" /> and
+        ///     {1} is the comma-separated list of SmartEnum values.
+        /// </param>
+        /// <exception cref="ArgumentNullException">When any of the constructor parameters are null.</exception>
+        /// <exception cref="InvalidOperationException">
+        ///     When <paramref name="smartEnumType" /> is not a
+        ///     <see cref="SmartEnum{TEnum}" /> or <see cref="SmartEnum{TEnum,TValue}" />
+        /// </exception>
+        public SmartEnumAttribute(Type smartEnumType, [CallerMemberName] string propertyName = null, bool allowCaseInsensitiveMatch = false,
+            string errorMessage = "{0} must be one of: {1}")
+        {
+            if (smartEnumType is null) throw new ArgumentNullException(nameof(smartEnumType));
+            if (propertyName is null) throw new ArgumentNullException(nameof(propertyName));
+            if (errorMessage is null) throw new ArgumentNullException(nameof(errorMessage));
+            List<string> smartEnumBaseTypes = new() { typeof(SmartEnum<>).Name, typeof(SmartEnum<,>).Name };
+            if (smartEnumType.BaseType == null || !smartEnumBaseTypes.Contains(smartEnumType.BaseType.Name))
+                throw new InvalidOperationException($"{nameof(smartEnumType)} must be a SmartEnum.");
+            _smartEnumType = smartEnumType;
+            _allowCaseInsensitiveMatch = allowCaseInsensitiveMatch;
+            ErrorMessage = string.Format(errorMessage, propertyName, string.Join(", ", GetValidSmartEnumValues()));
+        }
+
+        public override bool IsValid(object value)
+        {
+            if (value is not string name) return true;
+
+            return _allowCaseInsensitiveMatch
+                ? GetValidSmartEnumValues().Contains(name, StringComparer.InvariantCultureIgnoreCase)
+                : GetValidSmartEnumValues().Contains(name);
+        }
+
+        private List<string> GetValidSmartEnumValues()
+        {
+            List<string> values = new();
+            var typeWithList = _smartEnumType.BaseType!.Name == typeof(SmartEnum<>).Name
+                ? _smartEnumType.BaseType.BaseType!
+                : _smartEnumType.BaseType!;
+            var listProp = typeWithList.GetProperty("List", BindingFlags.Public | BindingFlags.Static);
+            var rawValue = listProp!.GetValue(null);
+            foreach (var val in (IEnumerable)rawValue!)
+            {
+                var namePropInfo = val.GetType().GetProperty("Name", BindingFlags.Public | BindingFlags.Instance);
+                var value = namePropInfo!.GetValue(val);
+                if (value is string valName) values.Add(valName);
+            }
+            return values;
+        }
+    }
+}
---
+++ test/SmartEnum.UnitTests/SmartEnumAttributeTests.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using FluentAssertions;
+using FluentAssertions.Execution;
+using Xunit;
+
+namespace Ardalis.SmartEnum.UnitTests
+{
+    public class SmartEnumAttributeTests
+    {
+        [Fact]
+        public void ThrowsWhenCtorGetsNullType()
+        {
+            SmartEnumAttribute CtorCall()
+            {
+                return new SmartEnumAttribute(null);
+            }
+
+            Assert.Throws<ArgumentNullException>(CtorCall);
+        }
+
+        [Fact]
+        public void ThrowsWhenCtorGetsNullPropertyName()
+        {
+            SmartEnumAttribute CtorCall()
+            {
+                return new SmartEnumAttribute(typeof(TestSmartEnum), propertyName: null, errorMessage: "Some Error Message");
+            }
+
+            Assert.Throws<ArgumentNullException>(CtorCall);
+        }
+
+        [Fact]
+        public void ThrowsWhenCtorGetsNullErrorMessage()
+        {
+            SmartEnumAttribute CtorCall()
+            {
+                return new SmartEnumAttribute(typeof(TestSmartEnum), errorMessage: null);
+            }
+
+            Assert.Throws<ArgumentNullException>(CtorCall);
+        }
+
+        [Fact]
+        public void ThrowsWhenCtorGetsNonSmartEnumType()
+        {
+            SmartEnumAttribute CtorCall()
+            {
+                return new SmartEnumAttribute(typeof(TestValidationModel));
+            }
+
+            Assert.Throws<InvalidOperationException>(CtorCall);
+        }
+
+        [Fact]
+        public void ReturnsErrorMessageContainingPropertyNameAndAllPossibleSmartEnumValues()
+        {
+            var model = new TestValidationModel { SomeProp = "foo" };
+            var validationContext = new ValidationContext(model, null, null);
+            List<ValidationResult> validationResults = new List<ValidationResult>();
+
+            Validator.TryValidateObject(model, validationContext, validationResults, true);
+
+            using (new AssertionScope())
+            {
+                validationResults.Should().HaveCount(1);
+                validationResults.Single().ErrorMessage.Should().Contain(nameof(TestValidationModel.SomeProp));
+                foreach (var name in TestSmartEnum.List.Select(cqo => cqo.Name))
+                    validationResults.Single().ErrorMessage.Should().Contain(name);
+            }
+        }
+
+        [Fact]
+        public void ValidatesGivenNonString()
+        {
+            var attribute = new SmartEnumAttribute(typeof(TestSmartEnum));
+            object nonString = new { };
+
+            var isValid = attribute.IsValid(nonString);
+
+            isValid.Should().BeTrue();
+        }
+
+        [Fact]
+        public void ValidatesGivenNullString()
+        {
+            var attribute = new SmartEnumAttribute(typeof(TestSmartEnum));
+
+            var isValid = attribute.IsValid(null);
+
+            isValid.Should().BeTrue();
+        }
+
+        [Fact]
+        public void ValidatesForEachMemberOfAGivenSmartEnum()
+        {
+            var attribute = new SmartEnumAttribute(typeof(TestSmartEnum));
+            using (new AssertionScope())
+            {
+                foreach (var addressTypeName in TestSmartEnum.List.Select(at => at.Name))
+                {
+                    var isValid = attribute.IsValid(addressTypeName);
+                    isValid.Should().BeTrue();
+                }
+            }
+        }
+
+        [Fact]
+        public void ValidatesForCaseInsensitiveStringWhenCaseInsensitiveMatchingEnabled()
+        {
+            var attribute = new SmartEnumAttribute(typeof(TestSmartEnum), allowCaseInsensitiveMatch: true);
+            var caseInsensitiveSource = TestSmartEnum.TestFoo.Name.ToLower();
+
+            var isValid = attribute.IsValid(caseInsensitiveSource);
+
+            isValid.Should().BeTrue();
+        }
+
+        [Fact]
+        public void DoesNotValidateForCaseInsensitiveStringWhenCaseInsensitiveMatchingDisabled()
+        {
+            var attribute = new SmartEnumAttribute(typeof(TestSmartEnum));
+            var caseInsensitiveSource = TestSmartEnum.TestFoo.Name.ToLower();
+
+            var isValid = attribute.IsValid(caseInsensitiveSource);
+
+            isValid.Should().BeFalse();
+        }
+
+        [Theory]
+        [InlineData("   ")]
+        [InlineData("Some Wrong Value")]
+        [InlineData("25")]
+        public void DoesNotValidateGivenNonSmartEnumNames(string invalidName)
+        {
+            var attribute = new SmartEnumAttribute(typeof(TestSmartEnum));
+
+            var isValid = attribute.IsValid(invalidName);
+
+            isValid.Should().BeFalse();
+        }
+
+        [Fact]
+        public void DoesNotThrowForSmartEnumWithDifferentKeyType()
+        {
+            // ReSharper disable once ObjectCreationAsStatement
+            Action ctorCall = () => new SmartEnumAttribute(typeof(TestSmartEnumString));
+
+            ctorCall.Should().NotThrow();
+        }
+
+        private class TestValidationModel
+        {
+            [SmartEnum(typeof(TestSmartEnum))]
+            public string SomeProp { get; set; }
+        }
+
+        private class TestSmartEnum : SmartEnum<TestSmartEnum>
+        {
+            public static readonly TestSmartEnum TestFoo = new TestSmartEnum(nameof(TestFoo), 2);
+
+            private TestSmartEnum(string name, int value) : base(name, value) { }
+        }
+
+        private class TestSmartEnumString : SmartEnum<TestSmartEnumString, string>
+        {
+            private TestSmartEnumString(string name, string value) : base(name, value) { }
+        }
+    }
+}
