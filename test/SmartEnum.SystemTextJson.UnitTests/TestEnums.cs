69d3ef44a5154931a75666447ab164b8b3be16e7
Steve Schafer <steve@donacobi.us>
2023-11-18 13:06:35 -0500

    Support SmartEnums as dictionary keys

--- test/SmartEnum.SystemTextJson.UnitTests/TestEnums.cs
+++ test/SmartEnum.SystemTextJson.UnitTests/TestEnums.cs
@@ -1,4 +1,8 @@
-﻿namespace Ardalis.SmartEnum.SystemTextJson.UnitTests
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Ardalis.SmartEnum.SystemTextJson.UnitTests
 {
     public sealed class TestEnumBoolean : SmartEnum<TestEnumBoolean, bool>
     {
@@ -33,5 +37,53 @@
         public static readonly TestEnumString Instance = new TestEnumString(nameof(Instance), "1.5");
 
         TestEnumString(string name, string value) : base(name, value) { }
+    }
+
+    public sealed class TestJsonConverters
+    {
+        public static readonly JsonSerializerOptions NameConverterOptions = new()
+        {
+            Converters =
+            {
+                new SmartEnumNameConverter<TestEnumInt32, int>(),
+                new SmartEnumNameConverter<TestEnumString, string>()
+            },
+            WriteIndented = true
+        };
+
+        public static readonly JsonSerializerOptions ValueConverterOptions = new()
+        {
+            Converters =
+            {
+                new SmartEnumValueConverter<TestEnumInt32, int>(),
+                new SmartEnumValueConverter<TestEnumString, string>()
+            },
+            WriteIndented = true
+        };
+
+    }
+
+    public sealed class TestDictInt32EnumString
+    {
+        public static readonly IDictionary<TestEnumInt32, string> Instance = new Dictionary<TestEnumInt32, string>
+            { { TestEnumInt32.Instance, nameof(Instance) } };
+    }
+
+    public sealed class TestDictStringEnumString
+    {
+        public static readonly IDictionary<TestEnumString, string> Instance = new Dictionary<TestEnumString, string>
+            { { TestEnumString.Instance, nameof(Instance) } };
+    }
+
+    public sealed class DictInt32EnumStringJson
+    {
+        [JsonPropertyName("1")]
+        public string Value => nameof(TestDictInt32EnumString.Instance);
+    }
+
+    public sealed class DictStringEnumStringJson
+    {
+        [JsonPropertyName("1.5")]
+        public string Value => nameof(TestDictInt32EnumString.Instance);
     }
 }
